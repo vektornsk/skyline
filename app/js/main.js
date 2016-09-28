ef69b651d8ee2b1182df239dbf722b66f4bc1176
Vasiliy P <vektornsk@yandex.ru>
2016-09-28 13:27:29 +0700

    card-index, box-info, carousel

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,12 @@
 $(function() {
 	/*carousel index*/
 	$('.js-carousel').slick({
-		arrow: false,
+		infinite: true,
+		fade: true,
+		arrows: false,
+		autoplay: true,
+	  autoplaySpeed: 3000,
+		dots: true,
 	});
 
 
